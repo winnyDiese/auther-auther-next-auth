654f38235f02d93d84fec23be5e55a3abb382020
winnyDiese <winnydiese@gmail.com>
2023-12-18 21:49:47 +0100

    creating route/page members

--- app/members/page.jsx
+++ app/members/page.jsx
@@ -1,11 +1,26 @@
 "use client"
-import {useSession} from "next-auth/react"
+import {signOut, useSession} from "next-auth/react"
+import { redirect } from "next/dist/server/api-utils"
 
 const Page = () => {
-    // const {data:session} = useSession()
+    const {data:session} = useSession()
+    if(!session) redirect("/sign-in")
+
+    const handleLogout = ()=> {
+        signOut()
+    }
+
     return (
         <div>
-            Members
+            Members Page
+
+            <p> {session?.user?.fullname}</p>
+            <button 
+                onClick={handleLogout}
+                className="bg-black text-white p-3"
+            >
+                Logout
+            </button>
         </div>
     );
 }
